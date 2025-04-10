d64ad0c3ed00274c933fba13a8ed416a7e8fca1d
sabenih726 <sabenih726@gmail.com>
2025-04-10 12:27:08 +0700

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,11 @@
-// Kosong untuk sekarang
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// Render aplikasi ke dalam elemen dengan id 'app'
+ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
