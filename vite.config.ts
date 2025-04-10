964b74f381f4e5aa4e1d41dae576cd26e1903bd2
sabenih726 <sabenih726@gmail.com>
2025-04-10 14:45:02 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Mengarahkan output build ke folder dist
-    assetsDir: 'assets', // Menyimpan file aset seperti gambar, css, js ke dalam folder assets
+    outDir: 'dist',
   },
-  publicDir: 'public', // Folder statis untuk file-file yang tidak terkompilasi (seperti favicon, logo, dll)
-  server: {
-    open: true, // Otomatis membuka browser ketika server dimulai
-  },
+  publicDir: 'public',
 });
