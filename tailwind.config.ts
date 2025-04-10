9ca7a703057e50ae7ce9247934798537a87748be
sabenih726 <sabenih726@gmail.com>
2025-04-10 14:45:19 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from 'tailwindcss';
-
-export default defineConfig({
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",  // pastikan semua file JS/TS dalam src dicakup
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-});
+};
