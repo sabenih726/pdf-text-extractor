8efaa7987d65d6a7fc6456e9b6a400bc5ee87f7f
sabenih726 <sabenih726@gmail.com>
2025-04-10 14:06:51 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist', // Mengarahkan output build ke folder dist
+    assetsDir: 'assets', // Menyimpan file aset seperti gambar, css, js ke dalam folder assets
   },
-  publicDir: 'public', // Folder statis
+  publicDir: 'public', // Folder statis untuk file-file yang tidak terkompilasi (seperti favicon, logo, dll)
+  server: {
+    open: true, // Otomatis membuka browser ketika server dimulai
+  },
 });
