893eaa18223ab5e5c64222f1cb1aa294f686d8c1
sabenih726 <sabenih726@gmail.com>
2025-04-10 12:30:16 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import PdfUploader from './components/PdfUploader';
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <PdfUploader />
     </div>
   );
